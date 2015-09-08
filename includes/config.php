c0aafa96fd0f4e86976f607e958f7e1779dc9eed
David Gaskin <davidburkegaskin@gmail.com>
2015-09-08 17:30:48 -0500

    Prepared DB connection on ASO
    Solo falta contrasena

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,18 @@
 <?php
 
-// database constants
+/* Database Constants */
+//test config
 defined("DB_SERVER") 	? NULL : define("DB_SERVER",'localhost');
 defined("DB_USER") 		? NULL : define("DB_USER",'admin');
 defined("DB_PASS") 		? NULL : define("DB_PASS",'');
 defined("DB_NAME") 		? NULL : define("DB_NAME",'songfarm-jul2015');
+
+// live config
+// defined("DB_SERVER") 	? NULL : define("DB_SERVER",'localhost');
+// defined("DB_USER") 		? NULL : define("DB_USER",'songfarm_david');
+// defined("DB_PASS") 		? NULL : define("DB_PASS",'');
+// defined("DB_NAME") 		? NULL : define("DB_NAME",'songfarm_registree_info');
+
 
 // Upload Errors
 defined("UPLOAD_ERR_OK") 					? NULL : define("UPLOAD_ERR_OK", 0);
